5b4eb536448baab4a712f0727c1683e973894a78
syo-sa1982 <fujii1308@gmail.com>
2015-05-11 02:50:30 +0900

    初期表示を限定してもっと見るボタン追記

--- decksample/js/deck_app.js
+++ decksample/js/deck_app.js
@@ -2,13 +2,16 @@
  * Created by yosuke.fujii on 2015/05/07.
  */
 function DeckApp (el){
-	var view = new DeckView(el),
-		url = 'https://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json',
-		self = this;
+	this.view = new DeckView(el);
+	this.url = 'https://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json';
 
-	this.fetch(url).then(function(data) {
-		self.data = data['data'];
-		view.render(self.data);
+	var self = this;
+
+	this.fetch(self.url).then(function(fetchData) {
+
+		self.data = fetchData.data;
+//		self.sendView(self);
+		self.view.render(self.data);
 	}, function(e) {
 		console.error("取得失敗");
 	});
@@ -21,3 +24,27 @@ DeckApp.prototype.fetch = function (url){
 	});
 };
 
+DeckApp.prototype.sendView = function (self,sendedData){
+
+	console.log(typeof data);
+	var dataLength = self.data.length,
+		resultData = {},
+		cnt = 0;
+	for(var i in self.data){
+		if (self.loadNum <= cnt){break;}
+		console.log(i);
+
+		resultData[i] = self.data[i];
+		cnt++;
+	}
+
+	console.log(typeof resultData);
+	console.log(resultData.length);
+
+	self.view.render(resultData);
+};
+
+
+DeckApp.prototype.isEmpty = function(value) {
+	return value === "";
+};
--- decksample/js/deck_view.js
+++ decksample/js/deck_view.js
@@ -4,18 +4,28 @@
 function DeckView(el) {
 
 	this.initialize(el);
-	this.handleEvents();
 }
 
 DeckView.prototype.initialize = function(el) {
+	this.loadNum = 20;
 	this.template = _.template($('[data-template="item"]').html());
 	this.imgUrl = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/';
+	this.asc = false; // 昇順(true)/降順(false)
+	this.nowSortKey = ""; // 現在ソートキー
+	this.initSortKey = "Total"; // 初期ソートキー
+
 	this.el = el;
+	this.$moreBtn = $("#more-btn");
+	this.handleEvents();
 };
 
 DeckView.prototype.handleEvents = function() {
 	var self = this;
 
+	this.$moreBtn.on("click", function(e){
+		self.loadNum += 8;
+		self.render(self.data);
+	});
 
 };
 
@@ -23,9 +33,28 @@ DeckView.prototype.onScroll = function(e) {
 };
 
 DeckView.prototype.render = function(data) {
+	this.data = data;
 	console.log(data);
 	var html = this.template({
-		list: data
+		list: this.dataFormat(data)
 	});
 	$(this.el).html(html);
+};
+
+
+DeckView.prototype.dataFormat = function (data,sendedData){
+
+	var self = this,
+		dataLength = data.length,
+		resultData = {},
+		cnt = 0;
+
+	for(var i in data){
+		if (self.loadNum <= cnt){break;}
+		console.log(i);
+
+		resultData[i] = data[i];
+		cnt++;
+	}
+	return resultData;
 };
