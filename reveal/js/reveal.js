babcb2a481b680c0c76283e25d7d1f5c802941ff
syo-sa1982 <fujii1308@gmail.com>
2016-01-07 17:37:45 +0900

    拡大率調整

--- reveal/js/reveal.js
+++ reveal/js/reveal.js
@@ -27,7 +27,7 @@ var Reveal = (function(){
 
 			// Bounds for smallest/largest possible scale to apply to content
 			minScale: 0.2,
-			maxScale: 1.0,
+			maxScale: 2.0,
 
 			// Display controls in the bottom right corner
 			controls: true,
